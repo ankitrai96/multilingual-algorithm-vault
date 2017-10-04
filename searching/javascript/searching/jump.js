ffa3d6e60d6579effd37f27ea158f625dd38c064
Jagdish Singh <jagdish.chauhan01@gmail.com>
2017-10-04 19:39:48 +0530

    index updated, jump search added, average time is displayed

---
+++ searching/javascript/searching/jump.js
@@ -0,0 +1,32 @@
+function jump(arr,x,n){
+    // Finding block size to be jumped
+    var step = Math.sqrt(n);
+ 
+    // Finding the block where element is
+    // present (if it is present)
+    var prev = 0;
+    while (arr[Math.min(step, n)-1] < x){
+        prev = step;
+        step += Math.sqrt(n);
+        if (prev >= n){
+            return "element does not exists";
+        }
+    }
+ 
+    // Doing a linear search for x in block
+    // beginning with prev.
+    while (arr[prev] < x){
+        prev++;
+ 
+        // If we reached next block or end of
+        // array, element is not present.
+        if (prev == Math.min(step, n))
+            return "element does not exists";
+    }
+    // If element is found
+    if (arr[prev] == x){
+        return "find at first index "+prev;
+    }
+    
+    return "element does not exists";
+}
